a91370a04097be17f6ccdd21014ba480a36ab0cd
Brian <briangondo@gmail.com>
2021-09-19 10:58:24 -0700

    first changes commit

--- server.js
+++ server.js
@@ -35,5 +35,12 @@ io.on("connection", socket => {
     });
 });
 
+if (process.env.PROD) {
+    app.use(express.static(path.join (__dirname, './client/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './client/build/index.html'));
+    });
+}
+
 const port = process.env.PORT || 8000;
 server.listen(port, () => console.log(`server is running on port ${port}`));
